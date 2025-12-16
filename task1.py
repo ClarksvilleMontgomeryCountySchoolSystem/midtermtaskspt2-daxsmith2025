a2ee067b51540121406f3ca262a303dba6b33f52
daxsmith2025 <daxton.smith795@students.cmcss.net>
2025-12-16 13:17:24 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -8,10 +8,10 @@ bagC = 19
 
 # Part 1: Combine the haul
 total_candy = bagA + bagB + bagC
-print(f"Total candy collected: {total_candy}")
+print(f'Total candy collected: {total_candy}')
 
 # Part 2: Fair sharing (include yourself)
-# TODO update people  to include yourself.
+total_people = people + 1
 # TODO Calculate share
 # TODO Calculate leftover
 # TODO Output Each person gets: #
