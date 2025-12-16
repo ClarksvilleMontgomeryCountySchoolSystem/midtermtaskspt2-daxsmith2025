932908b47ef09f3435d34e0ecc2fb8183ea06a81
daxsmith2025 <daxton.smith795@students.cmcss.net>
2025-12-16 10:11:16 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,28 @@
+# Halloween Candy Sharing
 
+# Given Variables
+people = 3  # friends who went trick-or-treating
+bagA = 23
+bagB = 17
+bagC = 19
+
+# Part 1: Combine the haul
+total_candy = bagA + bagB + bagC
+print(f'{total_candy}')
+
+
+# Part 2: Fair sharing (include yourself)
+# TODO update people  to include yourself.
+# TODO Calculate share
+# TODO Calculate leftover
+# TODO Output Each person gets: #
+# TODO Output: Leftover candy: #
+
+
+# Part 3: Include the sick friend
+# Variable reassignment is fine - previous values were already printed
+# TODO Update people to include sick friend.
+# TODO Recalculate share
+# TODO Recalculate leftover
+# TODO Output: Each person gets: #
+# TODO Output: Leftover candy: #
